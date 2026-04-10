32d32965db34dc1343efbc88a8ff914d5f6ea771
Khushi Tatad <kushiben8@gmail.com>
2026-04-10 23:38:29 +0530

    "Function Code Challenge W3 "

---
+++ function.py
@@ -0,0 +1,21 @@
+def my_func():
+    print("Hello From Function")
+
+# my_func()
+
+def greet(name):
+    print(name,"Khushi")
+
+# greet("Emil")
+
+# for i in range(1,11,2):
+    # print(i)
+
+
+
+# for i in range(6):
+#     print(i)
+
+
+for x in range(2,6):
+    print(x)
